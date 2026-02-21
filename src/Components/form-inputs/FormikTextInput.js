6d39a5d7cd8a60ad04e0d8159aceb28e0314cf5d
Josh Love <jdldeveloper@gmail.com>
2026-02-21 14:38:30 -0600

    Fix tests

--- src/Components/form-inputs/FormikTextInput.js
+++ src/Components/form-inputs/FormikTextInput.js
@@ -1,15 +1,17 @@
 import { TextField } from '@mui/material';
-import { useField } from 'formik';
+import { useField, useFormikContext } from 'formik';
 
 export const FormikTextInput = ({ label, id = null, ...props }) => {
   const [field, meta] = useField(props);
+  const { submitCount } = useFormikContext();
+  const showError = (meta.touched || submitCount > 0) && !!meta.error;
 
   return (
     <TextField
       fullWidth
       label={label}
-      error={meta.touched && !!meta.error}
-      helperText={meta.touched && meta.error}
+      error={showError}
+      helperText={showError ? meta.error : undefined}
       {...field}
       {...props}
       {...(id && { id: id })}
