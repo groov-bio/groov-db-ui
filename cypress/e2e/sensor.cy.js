521452a7542729f48c4fd68ef28f43361e080a32
Josh Love <jdldeveloper@gmail.com>
2026-04-12 12:39:48 -0500

    Click changes

--- cypress/e2e/sensor.cy.js
+++ cypress/e2e/sensor.cy.js
@@ -33,17 +33,17 @@ describe('Sensor Page', () => {
 
     cy.visit('http://localhost:3000');
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('#Browse').click();
+    cy.get('#Browse').click({force: true});
     cy.get('#database-link-TetR').click({force: true});
-    cy.get('[data-id="U2Y8G0"] [data-field="uniprot"] a').click();
-    cy.get('#sensor-page-tab-view-switch').click();
+    cy.get('[data-id="U2Y8G0"] [data-field="uniprot"] a').click({force: true});
+    cy.get('#sensor-page-tab-view-switch').click({force: true});
     cy.get('#sensor-about').should(
       'have.text',
       'Regulates the expression of the ecd operon responsible for the degradation of estradiol.'
     );
     cy.get("#sensor-metadata-family").should('have.text', "Family: TETR")
     cy.get("#metadata-table-data-Organism").should('have.text', "Caenibius tardaugens")
-    cy.get("#sensor-ligands-tab").click()
+    cy.get("#sensor-ligands-tab").click({force: true})
     cy.get('#SMILEScanvas').should('have.id', 'SMILEScanvas');
     cy.get('#SMILEScanvas').should('be.visible');
     cy.get(
@@ -74,7 +74,7 @@ describe('Sensor Page', () => {
       'have.text',
       'MethodSynthetic regulation'
     );
-    cy.get("#sensor-genomes-tab").click()
+    cy.get("#sensor-genomes-tab").click({force: true})
     cy.get('.konvajs-content > canvas').should('be.visible');
     cy.get(
       "#enzyme-chip > span"
